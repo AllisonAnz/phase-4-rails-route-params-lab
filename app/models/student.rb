01e2a9b47b89e335745f2262fa52bacdc0080c91
AllisonAnz <AllisonAnzalone@gmail.com>
2021-10-11 15:47:33 -0500

    commit tree 1a44862726e445e376c192eb86348c58756c6151

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,33 @@
+#student.rb
 class Student < ApplicationRecord
+  # class method written using a scope (https://www.rubyguides.com/2019/10/scopes-in-ruby-on-rails/)
+  scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+  
+  # class method written without using a scope
+  # def self.by_name(name)
+  #   Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+  # end
 
   def to_s
     "#{first_name} #{last_name}"
   end
-
 end
+
+#------------------------------------------------------------------
+# students_controller.rb
+class StudentsController < ApplicationController
+  def index
+    students = if params[:name]
+                 Student.by_name(params[:name])
+               else
+                 Student.all
+               end    
+    render json: students
+  end
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+end
+
